03010366e5d0271050f1ccff2c6fe149f5ba3c35
dinh.thuy.duong <dinh.thuy.duong@fujitsu.com>
2020-06-15 16:54:07 +0900

    Update instrumental test case

--- app/src/androidTest/java/mbaas/com/nifcloud/androidncmbgame/ApplicationTest.java
+++ app/src/androidTest/java/mbaas/com/nifcloud/androidncmbgame/ApplicationTest.java
@@ -1,48 +1,10 @@
 package mbaas.com.nifcloud.androidncmbgame;
-import android.os.Parcel;
-import android.util.Pair;
-import androidx.test.filters.SmallTest;
-import androidx.test.runner.AndroidJUnit4;
 import com.google.common.truth.Truth.assertThat;
-import java.util.List;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-// @RunWith is required only if you use a mix of JUnit3 and JUnit4.
-@RunWith(AndroidJUnit4.class)
-@SmallTest
 public class ApplicationTest {
-
-    public static final String TEST_STRING = "This is a string";
-    public static final long TEST_LONG = 12345678L;
-    private LogHistory mLogHistory;
-
-    @Before
-    public void createLogHistory() {
-        mLogHistory = new LogHistory();
-    }
-
     @Test
-    public void logHistory_ParcelableWriteRead() {
-        // Set up the Parcelable object to send and receive.
-        mLogHistory.addEntry(TEST_STRING, TEST_LONG);
-
-        // Write the data.
-        Parcel parcel = Parcel.obtain();
-        mLogHistory.writeToParcel(parcel, mLogHistory.describeContents());
-
-        // After you're done with writing, you need to reset the parcel for reading.
-        parcel.setDataPosition(0);
-
-        // Read the data.
-        LogHistory createdFromParcel = LogHistory.CREATOR.createFromParcel(parcel);
-        List<Pair<String, Long>> createdFromParcelData
-                = createdFromParcel.getData();
-
-        // Verify that the received data is correct.
-        assertThat(createdFromParcelData.size()).isEqualTo(1);
-        assertThat(createdFromParcelData.get(0).first).isEqualTo(TEST_STRING);
-        assertThat(createdFromParcelData.get(0).second).isEqaulTo(TEST_LONG);
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertThat(EmailValidator.isValidEmail("name@email.com")).isTrue();
     }
 }
