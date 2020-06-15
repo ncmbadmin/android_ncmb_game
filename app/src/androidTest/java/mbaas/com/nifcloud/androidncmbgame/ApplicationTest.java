076e9a33a6c233e763e24d953dcf740e33cda61e
dinh.thuy.duong <dinh.thuy.duong@fujitsu.com>
2020-06-15 16:46:57 +0900

    Update test

--- app/src/androidTest/java/mbaas/com/nifcloud/androidncmbgame/ApplicationTest.java
+++ app/src/androidTest/java/mbaas/com/nifcloud/androidncmbgame/ApplicationTest.java
@@ -1,13 +1,48 @@
 package mbaas.com.nifcloud.androidncmbgame;
+import android.os.Parcel;
+import android.util.Pair;
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
+import com.google.common.truth.Truth.assertThat;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+// @RunWith is required only if you use a mix of JUnit3 and JUnit4.
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class ApplicationTest {
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+    public static final String TEST_STRING = "This is a string";
+    public static final long TEST_LONG = 12345678L;
+    private LogHistory mLogHistory;
+
+    @Before
+    public void createLogHistory() {
+        mLogHistory = new LogHistory();
+    }
+
+    @Test
+    public void logHistory_ParcelableWriteRead() {
+        // Set up the Parcelable object to send and receive.
+        mLogHistory.addEntry(TEST_STRING, TEST_LONG);
+
+        // Write the data.
+        Parcel parcel = Parcel.obtain();
+        mLogHistory.writeToParcel(parcel, mLogHistory.describeContents());
+
+        // After you're done with writing, you need to reset the parcel for reading.
+        parcel.setDataPosition(0);
+
+        // Read the data.
+        LogHistory createdFromParcel = LogHistory.CREATOR.createFromParcel(parcel);
+        List<Pair<String, Long>> createdFromParcelData
+                = createdFromParcel.getData();
+
+        // Verify that the received data is correct.
+        assertThat(createdFromParcelData.size()).isEqualTo(1);
+        assertThat(createdFromParcelData.get(0).first).isEqualTo(TEST_STRING);
+        assertThat(createdFromParcelData.get(0).second).isEqaulTo(TEST_LONG);
     }
 }
